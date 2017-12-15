7c92f1ccf7022013f12b5592af7b5c40b13b1b7c
oloomi <smh.oloomi@gmail.com>
2017-12-15 20:05:01 +1100

    remu.py with command line argument parsing added. evaluation.py added and evaluation codes moved to this file.

---
+++ remu.py
@@ -0,0 +1,33 @@
+
+"""
+Copyright (C) 2017 S Mohammad H Oloomi (smh.oloomi@gmail.com)
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
+import bayesian_update as bu
+import argparse
+import textwrap
+
+parser = argparse.ArgumentParser(usage='\n\tpython3 remu.py -i <input.sam> -r <reference.fa> [-o <output.sam>]\n' +
+                                       'help:\n\tpython3 remu.py -h',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=textwrap.dedent('''\
+                                 REMU: REsolving MUltimappings in Read Mapping
+                                 by Mohammad Oloomi (smh.oloomi@gmail.com)\n'''),
+                                 epilog='')
+parser.add_argument('-i', '--input', required=True, help='Input SAM file that contains all read mappings')
+parser.add_argument('-r', '--reference', required=True, help='Reference genome in FASTA format')
+parser.add_argument('-o', '--output', default='output.sam',
+                    help='Output SAM file that contains correct mappings (default: output.sam)')
+# Software version
+parser.add_argument('-v', '--version', action='version', version='REMU 1.0.0', help='Software version')
+args = parser.parse_args()
+
+if args.input and args.reference:
+    print(parser.description)
+    bu.bayesian_update(args.reference, args.input, args.output)
+
+
+# -r "./data/genomes/toy-genome.fna" -i "./read-mapping/toy-genome-mutated/toy-wg-mutated-se-mapping-report-all.sam" -o "./read-mapping/toy-genome-mutated/test.sam"
