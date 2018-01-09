d482ccccc8c0a732322e11f23bb348a31f428028
oloomi <smh.oloomi@gmail.com>
2018-01-09 19:07:52 +1100

    WIP: REMU bash-script problem

--- evaluation/bash-scripts/multimapping-resolution.sh
+++ evaluation/bash-scripts/multimapping-resolution.sh
@@ -19,7 +19,7 @@ outfile="./mappings/bowtie/bowtie"
 echo "\n=== Bowtie + MMR Multimapping Resolution Done ===\n"
 
 # REMU method
-/usr/bin/time -v remu.py -g $reference -i $alignments.sam -o $outfile-remu.sam -r 50
+sudo /usr/bin/time -v remu.py -g $reference -i $alignments.sam -o $outfile-remu.sam -r 50
 
 sam_file=$outfile-remu
 samtools view -bS $sam_file.sam -o $sam_file.bam
@@ -29,17 +29,17 @@ samtools index $sam_file-sorted.bam
 echo "\n=== Bowtie + REMU Multimapping Resolution Done ===\n"
 
 # ----------- BWA ----------
-alignments="./mappings/bwa/bwa-mapping-report-all-sorted"
-outfile="./mappings/bwa/bwa"
-
-# MMR method
-samtools sort -n $alignments.bam -o $alignments-id-sorted.bam
-
-/usr/bin/time -v mmr -o $outfile-mmr.bam -F 3 -b -R 150 $alignments-id-sorted.bam > log-mmr-bwa.txt
-
-sam_file=$outfile-mmr
-samtools sort $sam_file.bam -o $sam_file-sorted.bam
-samtools index $sam_file-sorted.bam
+#alignments="./mappings/bwa/bwa-mapping-report-all-sorted"
+#outfile="./mappings/bwa/bwa"
+#
+## MMR method
+#samtools sort -n $alignments.bam -o $alignments-id-sorted.bam
+#
+#/usr/bin/time -v mmr -o $outfile-mmr.bam -F 3 -b -R 150 $alignments-id-sorted.bam > log-mmr-bwa.txt
+#
+#sam_file=$outfile-mmr
+#samtools sort $sam_file.bam -o $sam_file-sorted.bam
+#samtools index $sam_file-sorted.bam
 
 echo "\n=== BWA + MMR Multimapping Resolution Done ===\n"
 
