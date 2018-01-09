287734dd026b9a8640579094e0bb2d2fb2b409fb
oloomi <smh.oloomi@gmail.com>
2018-01-09 19:05:37 +1100

    WIP: REMU bash-script problem

--- evaluation/bash-scripts/multimapping-resolution.sh
+++ evaluation/bash-scripts/multimapping-resolution.sh
@@ -8,18 +8,18 @@ alignments="./mappings/bowtie/bowtie-mapping-report-all-sorted"
 outfile="./mappings/bowtie/bowtie"
 
 # MMR method
-samtools sort -n $alignments.bam -o $alignments-id-sorted.bam
-
-/usr/bin/time -v mmr -o $outfile-mmr.bam -F 3 -b -R 150 $alignments-id-sorted.bam > log-mmr-bowtie.txt
-
-sam_file=$outfile-mmr
-samtools sort $sam_file.bam -o $sam_file-sorted.bam
-samtools index $sam_file-sorted.bam
+#samtools sort -n $alignments.bam -o $alignments-id-sorted.bam
+#
+#/usr/bin/time -v mmr -o $outfile-mmr.bam -F 3 -b -R 150 $alignments-id-sorted.bam > log-mmr-bowtie.txt
+#
+#sam_file=$outfile-mmr
+#samtools sort $sam_file.bam -o $sam_file-sorted.bam
+#samtools index $sam_file-sorted.bam
 
 echo "\n=== Bowtie + MMR Multimapping Resolution Done ===\n"
 
 # REMU method
-/usr/bin/time -v remu.py -g $reference -i $alignments.sam -o $outfile-remu.sam -r 50 > log-remu-bowtie.txt
+/usr/bin/time -v remu.py -g $reference -i $alignments.sam -o $outfile-remu.sam -r 50
 
 sam_file=$outfile-remu
 samtools view -bS $sam_file.sam -o $sam_file.bam
