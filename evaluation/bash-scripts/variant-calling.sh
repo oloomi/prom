657f1c0728a476d2a098e8fbd6c2449e8e17091a
oloomi <smh.oloomi@gmail.com>
2018-01-14 23:03:54 +1100

    Bowtie variant calling uncommented.

--- evaluation/bash-scripts/variant-calling.sh
+++ evaluation/bash-scripts/variant-calling.sh
@@ -3,19 +3,19 @@
 reference="../../genome-ref/ref-genome.fna"
 out_path="./variants/"
 
-#file_path="./mappings/bowtie/"
-#alignment_files="bowtie-mapping-best-match-sorted
-#bowtie-mapping-report-all-sorted
-#bowtie-mmr-sorted
-#bowtie-remu-sorted"
-#
-#
-#for file in $alignment_files
-#do
-#	echo "\nVariant calling for: $file\n"
-#	freebayes -f $reference -p 1 -F 0.9 $file_path$file.bam >$out_path$file-variants-freebayes.vcf
-#done
-#
+file_path="./mappings/bowtie/"
+alignment_files="bowtie-mapping-best-match-sorted
+bowtie-mapping-report-all-sorted
+bowtie-mmr-sorted
+bowtie-remu-sorted"
+
+
+for file in $alignment_files
+do
+	echo "\nVariant calling for: $file\n"
+	freebayes -f $reference -p 1 -F 0.9 $file_path$file.bam >$out_path$file-variants-freebayes.vcf
+done
+
 file_path="./mappings/bwa/"
 alignment_files="bwa-mapping-best-match-sorted
 bwa-mapping-report-all-sorted
