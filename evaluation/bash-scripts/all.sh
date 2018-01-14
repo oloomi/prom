e766c543482e6a7c111fd50c072c0138e3e15abc
Ubuntu <ubuntu@instance1.novalocal>
2018-01-14 07:26:12 +0000

    read-mapping added to all.sh

--- evaluation/bash-scripts/all.sh
+++ evaluation/bash-scripts/all.sh
@@ -2,6 +2,7 @@
 # Running all scripts
 
 python3 /mnt/remu/evaluation/evaluation.py -c
+sh /mnt/remu/evaluation/bash-scripts/read-mapping.sh
 sh /mnt/remu/evaluation/bash-scripts/multimapping-resolution.sh
 sh /mnt/remu/evaluation/bash-scripts/variant-calling.sh
 python3 /mnt/remu/evaluation/evaluation.py -e
