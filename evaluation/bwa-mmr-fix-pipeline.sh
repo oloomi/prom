aa86ecf6919a31b3ee773693460034d68340f30f
oloomi <smh.oloomi@gmail.com>
2018-03-22 15:07:01 +1100

    Pipeline for fixing BWA+MMR issue added.

---
+++ evaluation/bwa-mmr-fix-pipeline.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dir=`pwd`/
+script_path="$( cd "$(dirname "$0")" ; pwd -P )/"
+
+# Running experiments
+cd ${dir}mtb/simulated-data/begin-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}mtb/simulated-data/middle-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}mtb/real-data/back-mutate
+sh ${script_path}bwa-mmr-fix.sh -r 150
+
+# Running experiments
+cd ${dir}ecoli/simulated-data/begin-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}ecoli/simulated-data/middle-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}ecoli/real-data/back-mutate
+sh ${script_path}bwa-mmr-fix.sh -r 150
+
+# Running experiments
+cd ${dir}yeast/simulated-data/begin-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}yeast/simulated-data/middle-supermax
+sh ${script_path}bwa-mmr-fix.sh -s 150
+
+cd ${dir}yeast/real-data/back-mutate
+sh ${script_path}bwa-mmr-fix.sh -r 100
