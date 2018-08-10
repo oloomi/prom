fba7cba3ae10871c6a5a34bfd5aedbfa8ef99f12
oloomi <smh.oloomi@gmail.com>
2018-08-10 21:23:19 +1000

    Update for getting reads from ENA database.

--- evaluation/get-data.sh
+++ evaluation/get-data.sh
@@ -26,15 +26,18 @@ find_repeats() {
   cd ../..
 }
 
+# run_id read_len dir
 get_reads() {
   fastq-dump --outdir $3/reads/fastq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip $1
   gunzip $3/reads/fastq/$1_pass_1.fastq.gz
   fastx_trimmer -l $2 -m $2 -Q33 -i $3/reads/fastq/$1_pass_1.fastq -o $3/reads/reads.fq
 }
 
+# run_id read_len dir url
 get_reads_ena() {
-  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/$1/$2/$2_1.fastq.gz -P $3/reads/fastq
-  gunzip -c $3/reads/fastq/$2_1.fastq.gz > $3/reads/reads.fq
+  wget $4 -P $3/reads/fastq
+  gunzip -c $3/reads/fastq/$1_1.fastq.gz
+  fastx_trimmer -l $2 -m $2 -Q33 -i $3/reads/fastq/$1_pass_1.fastq -o $3/reads/reads.fq
 }
 
 prepare_data() {
@@ -50,15 +53,15 @@ get_reads SRR2818101 150 mtb/real-data/back-mutate
 
 # Escherichia coli
 prepare_data ecoli 100 ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-get_reads ERR022075 100 yeast/real-data/back-mutate
-#get_reads_ena ERR022 ERR022075 yeast/real-data/back-mutate
+#get_reads ERR022075 100 yeast/real-data/back-mutate
+get_reads_ena ERR022075 100 yeast/real-data/back-mutate ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR022/ERR022075/ERR022075_1.fastq.gz
 mv yeast/real-data/back-mutate/reads.fq yeast/real-data/back-mutate/all-reads.fq
 seqtk sample -s100 yeast/real-data/back-mutate/all-reads.fq 2500000 > yeast/real-data/back-mutate/reads.fq
-#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR022/ERR022075/ERR022075_1.fastq.gz
 
 # Saccharomyces cerevisiae S288C (baker's yeast)
 prepare_data yeast 150 ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.fna.gz
-get_reads ERR1938683 150 yeast/real-data/back-mutate
+#get_reads ERR1938683 150 yeast/real-data/back-mutate
+get_reads_ena ERR022075 150 yeast/real-data/back-mutate ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR193/ERR1938683/ERR1938683_1.fastq.gz
 
 # Human chromosome 19 GRCh38.p7 assembly
 #prepare_data human-chr19 100 http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr19.fa.gz
